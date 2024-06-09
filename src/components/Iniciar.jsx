b1ddb2f2b74e6f4926d8df874529b789b2e0601a
ronesanttos <devronesantos@gmail>
2024-06-09 14:52:10 -0300

    paragrafo add

--- src/components/Iniciar.jsx
+++ src/components/Iniciar.jsx
@@ -5,7 +5,7 @@ const Iniciar = ({iniciarJogo}) => {
   return (
     <div className='start'>
         <h1>Palavra Secreta</h1>
-        <p>Testando git</p>
+        <p>Testando git + github</p>
         <p>Clique no botão para iniciar</p>
         <button onClick={iniciarJogo}>Começar jogo</button>
     </div>
